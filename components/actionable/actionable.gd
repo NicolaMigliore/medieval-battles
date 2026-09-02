7224c4c9cc59b3e1b391e8508d94b3c4baa86edc
n.migliore <n.migliore@blogic.it>
2026-09-02 15:05:54 +0200

    dev: added dialog portrait handling. Closes #6

--- components/actionable/actionable.gd
+++ components/actionable/actionable.gd
@@ -11,6 +11,5 @@ var _balloon_scene = preload("res://components/dialogue_balloon/balloon.tscn")
 
 func action() -> void:
 	DialogueManager.show_dialogue_balloon_scene(_balloon_scene, dialogue_resource, dialogue_start)
-	# DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
 	DialogueManager.dialogue_started.connect(func(_dialogue_resource):dialogue_started.emit())
 	DialogueManager.dialogue_ended.connect(func(_dialogue_resource):dialogue_ended.emit())
